6ccac52efe5cd3afab9aec1f148a9db044621d23
pepe_fabra_valverde <jofaval@outlook.com>
2022-10-06 22:15:42 +0200

    refactor: Implement a generic type

--- src/hooks/useTree/types/useTree.types.ts
+++ src/hooks/useTree/types/useTree.types.ts
@@ -6,19 +6,39 @@ export enum SelectAllStatusEnum {
   NONE = "NONE",
 }
 
-export type useTreeProps<TItem = any> = {
-  data: TItem[];
+export type useTreeProps<TGenerics extends TreeGenericsType> = {
+  data: TGenerics["Item"][];
   onMouseEnterItem: (e: Event) => void;
   searchDetails?: SearchDetailsType;
-  searchKey?: string;
+  /**
+   * It'll generate a Map, and for it to properly work, it needs and id for each element,
+   */
+  IdKey?: string;
+  /**
+   * The key inside the item that will identify it's parent, if possible
+   */
+  ParentKey?: string;
 };
 
-export type useTreeResponse<TItem = any, TKey = string> = {
+export type useTreeResponse<TGenerics extends TreeGenericsType> = {
   // allSelected: boolean;
   // getWrapper: () => JSX.Element;
-  onSelectionChanged: (keys: TKey[]) => void;
+  onSelectionChanged: (keys: TGenerics["Key"][]) => void;
 };
 
-export type useTreeType<TItem = any, TKey = string> = (
-  props: useTreeProps<TItem>
-) => useTreeResponse<TKey>;
+export type useTreeType<TGenerics extends TreeGenericsType> = (
+  props: useTreeProps<TGenerics>
+) => useTreeResponse<TGenerics>;
+
+export type TreeGenericsType = {
+  /**
+   * The type of the element, or item, that will be used all throughout the hook.
+   * This is the element that will be iterated, and it's properties, you can not define it,
+   * you don't even have to pass a type, but it'd be better if you would.
+   */
+  Item?: any;
+  /**
+   * The [ID] key type could be infered, but it'd be nicer if you were to explicitely provide it.
+   */
+  Key?: any;
+};
